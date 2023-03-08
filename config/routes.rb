1f1fdf7a3ae0802f51ae25a48d8f1c55654ac675
edyta pilorz <edytapilorz@gmail.com>
2023-03-08 13:20:59 +0100

    hopefully routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :gardens, only: [:show] do
+    member do
+      get 'email'
+    end
     resources :build, controller: 'gardens/build'
-  end
-
-  resources :plants, only: [:new, :create] do
-    collection do
-      get 'categories'
+    resources :plants, only: [:new, :create] do
+      collection do
+        get 'categories'
+      end
     end
   end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
