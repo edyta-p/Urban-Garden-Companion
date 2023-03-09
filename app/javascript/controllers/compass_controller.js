dc77450fa8a9e0279d030032d5f70e5029de3200
Jaouad Karroumi <ixyhoo@outlook.fr>
2023-03-09 11:37:41 +0100

    test compass

--- app/javascript/controllers/compass_controller.js
+++ app/javascript/controllers/compass_controller.js
@@ -2,29 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="compass"
 export default class extends Controller {
-  static targets = ["compassCircle", "startBtn", "myPoint", "arrow"];
+  static targets = ["compassCircle", "output"];
 
-
-  connect() {
-    console.log('je suis dans le controller stimulus')
-    console.log(this.arrowTarget)
-    console.log(this.compassCircleTarget)
-    console.log(this.startBtnTarget)
-    console.log(this.myPointTarget)
-    this.pointDegree;
-    this.compass
-
-  }
-
-  startCompass() {
-
-      window.addEventListener("deviceorientationabsolute", this.handler, true);
-
-  }
-
-  handler(e) {
-    this.compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    this.compassCircleTarget.style.transform = `translate(-50%, -50%) rotate(${-this.compass}deg)`;
-
+  update(event) {
+    this.compass = event.webkitCompassHeading || Math.abs(event.alpha - 360)
+    this.compassCircleTarget.style.transform = `translate(-50%, -50%) rotate(${-this.compass}deg)`
   }
 }
