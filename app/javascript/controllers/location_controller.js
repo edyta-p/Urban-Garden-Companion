6b2f54377c147ad58390b9f8f2af2638304debf1
luca van der voo <luca.voo@alunosp.colegiomaterdei.net>
2023-03-09 13:42:31 +0100

    Map finish

---
+++ app/javascript/controllers/location_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="location"
+export default class extends Controller {
+  static targets = ["map", "lat", "lng"];
+
+  connect() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition.bind(this));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    this.latTarget.innerHTML = lat;
+    this.lngTarget.innerHTML = lng;
+    const map = new google.maps.Map(this.mapTarget, {
+      center: { lat, lng },
+      zoom: 8
+    });
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: map
+    });
+  }
+}
