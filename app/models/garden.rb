59438d55d49a1137c6bb538c91761aece17cc6cb
edyta pilorz <edytapilorz@gmail.com>
2023-03-09 10:46:28 +0100

    radio buttons added

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,12 +2,18 @@ class Garden < ApplicationRecord
   has_many :plants_gardens
 
   CATEGORY = ['window ledge', 'balcony', 'terrace']
+  IMAGE_CATEGORY = {
+    'window ledge': 'https://www.minotti.com/media/immagini/27427_z_MINOTTI_TERRACE_001.jpg',
+    'balcony': 'https://www.minotti.com/media/immagini/27427_z_MINOTTI_TERRACE_001.jpg',
+    'terrace': 'https://www.minotti.com/media/immagini/27427_z_MINOTTI_TERRACE_001.jpg'
+  }
+
   EXPOSURE = ['N', 'S', 'W', 'E']
 
   validates :category, inclusion: { in: CATEGORY }, if: :active_or_category?
   validates :exposure, inclusion: { in: EXPOSURE }, if: :active_or_exposure?
-  validates :length, presence: true, if: :active_or_category?
-  validates :width, presence: true, if: :active_or_category?
+  validates :length, presence: true, if: :active_or_length?
+  validates :width, presence: true, if: :active_or_width?
   validates :plant_categories, presence: true, if: :active_or_plants?
 
   def active?
@@ -15,7 +21,15 @@ class Garden < ApplicationRecord
   end
 
   def active_or_category?
-    (status.include?('category') && status.include?('length') && status.include?('width')) || active?
+    status.include?('category') || active?
+  end
+
+  def active_or_length?
+    status.include?('length') || active?
+  end
+
+  def active_or_width?
+    status.include?('width') || active?
   end
 
   def active_or_exposure?
