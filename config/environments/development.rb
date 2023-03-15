d25f00062adf4d239a4b8ace49f6e0f73cd42485
edyta pilorz <edytapilorz@gmail.com>
2023-03-15 14:32:14 +0100

    mail working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -61,6 +61,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000", protocol: "http" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
