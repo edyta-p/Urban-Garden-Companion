5899718805fd77ef61d3ecb1b317cfcc078e6886
edyta pilorz <edytapilorz@gmail.com>
2023-03-15 11:53:49 +0100

    new mail

--- app/mailers/send_pdf_mailer.rb
+++ app/mailers/send_pdf_mailer.rb
@@ -2,7 +2,8 @@ class SendPdfMailer < ApplicationMailer
   def your_garden
     @email = params[:email]
     @garden = params[:garden]
-   mail(from: "benjbdk@gmail.com", to: @email, subject: 'Urban ')
+    mail(from: "urbangardencompanion1102@hotmail.com", to: @email, subject: 'Urban Garden Companion')
     # This will render a view in `app/views/user_mailer`!
+    # benjbdk@gmail.com
   end
 end
